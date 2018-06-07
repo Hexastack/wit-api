96af047295fda1f971f59274554679c7e8b7ed85
bashz <nemlaghi.bechir@gmail.com>
2018-06-07 17:36:21 +0100

    unit test on training, message & speech

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ describe('Wit', function () {
   })
 
   const wit = new Wit(token)
-  
+
   describe('Entity services', function () {
     describe('Get all entities', function () {
       it('Should return list of entites', function () {
@@ -134,7 +134,7 @@ describe('Wit', function () {
       })
     })
     it('Should add a value', function () {
-      return wit.value.add('country', 'Tunisia', ['Tunisia', 'etoile du nord'], {continent: 'africa'}).then((entity) => {
+      return wit.value.add('country', 'Tunisia', ['Tunisia', 'etoile du nord'], { continent: 'africa' }).then((entity) => {
         expect(entity).to.be.an(Entity)
         expect(entity.id).not.to.be.empty()
         expect(entity.lookups).to.contain('keywords')
@@ -174,6 +174,126 @@ describe('Wit', function () {
     })
   })
 
+  describe('Learning', function () {
+    describe('Train using one sample at a time', function () {
+      it('Should yeild on a json response', function () {
+        return wit.train('This is meant to be unlearned', [
+          {
+            entity: "country",
+            value: "Tunisia"
+          }
+        ]).then(resZero => {
+          return wit.train('Hello Tunisia', [
+            {
+              entity: "country",
+              value: "Tunisia",
+              start: 6,
+              end: 13
+            },
+            {
+              entity: "wit$greetings",
+              value: true,
+              start: 0,
+              end: 5
+            }
+          ]).then(resOne => {
+            return wit.train('Tunis says hi', [
+              {
+                entity: "country",
+                value: "Tunisia",
+                start: 0,
+                end: 5
+              },
+              {
+                entity: "wit$greetings",
+                value: true,
+                start: 11,
+                end: 13
+              }
+            ]).then(resTwo => {
+              expect(resOne).to.be.an(Object)
+              expect(resOne).to.be.eql(resTwo)
+              expect(resOne).to.be.eql(resZero)
+              expect(resOne.sent).to.be.ok()
+              expect(resOne.n).to.be.ok()
+            })
+          })
+        }).catch(e => {
+          console.log(e)
+        })
+      })
+    })
+    describe('Unlearn one sample', function () {
+      return wit.forget('This is meant to be unlearned').then(res => {
+        expect(res).to.be.an(Object)
+        expect(res.sent).to.be.ok()
+        expect(res.n).to.be.ok()
+      }).catch(e => {
+        console.log(e)
+      })
+    })
+  })
+
+  describe('Text messages', function () {
+    describe('Test on a learned text', function () {
+      it('Should respond by an Intent Object', function () {
+        const options = {
+          context: {
+            locale: 'en_US'
+          },
+          n: 4 // returns up to 4 possible intents
+        }
+        return wit.message('holla tunisia', options).then((intent) => {
+          expect(intent).to.be.an(Intent)
+          expect(intent).to.have.property('entities')
+          expect(intent.entities).not.to.be.empty()
+          expect(intent.entities).to.have.property('country')
+          expect(intent.entities).to.have.property('greetings')
+          const bestMatch = intent.maxConfidence()
+          expect(bestMatch).to.have.property('confidence')
+          expect(bestMatch).to.have.property('value')
+          expect(bestMatch).to.have.property('entity')
+        }).catch((error) => {
+          console.error(error)
+        })
+      })
+    })
+    describe('Test on a forgotten text', function () {
+      it('Should respond by an Intent Object containing greeting', function () {
+        const options = {
+          context: {
+            locale: 'en_US'
+          },
+          n: 1 // returns up to 4 possible intents
+        }
+        return wit.message('Hi This is meant to be unlearned', options).then((intent) => {
+          expect(intent).to.be.an(Intent)
+          expect(intent).to.have.property('entities')
+          expect(intent.entities).not.to.be.empty()
+          expect(intent.entities).to.have.property('greetings')
+          expect(intent.entities).not.to.have.property('country')
+        })
+      })
+    })
+  })
+
+  describe('Speech', function () {
+    it('Should respond by an Intent Object', function () {
+      return wit.speech('./test/sample.wav').then((intent) => {
+        expect(intent).to.be.an(Intent)
+        expect(intent._test).to.be('hello')
+        expect(intent.entities).not.to.be.empty()
+        expect(intent.entities).to.have.property('wit$greetings')
+        const bestMatch = intent.maxConfidence()
+        console.log(bestMatch)
+        expect(bestMatch).to.have.property('confidence')
+        expect(bestMatch).to.have.property('value')
+        expect(bestMatch).to.have.property('entity')
+      }).catch((error) => {
+        console.error(error)
+      })
+    })
+  })
   const defectedWit = new Wit('cant be a token')
   describe('Error handeling', function () {
     describe('Api fails', function () {
